2280d87bfb8d0ab3a9fc2bd80145641ffa674028
onaug6th <592986151@qq.com>
2019-01-16 11:42:20 +0800

    移除无用代码

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div ref="appRef" class="app">
+    <div class="app">
         
         <div ref="contentRef">
             <transition name="fade">
@@ -40,56 +40,3 @@ export default {
     }
 }
 </script>
-
-
-<style>
-    .top-nav{
-        width:100%;
-        position:fixed;
-        padding:0px;
-        top:0px;
-        z-index:3;
-        transition: all .3s;
-    }
-    .scroll{
-        background: hsla(0,0%,100%,.86);
-        box-shadow: 0 0 25px #ccc;
-        color: #999;
-    }
-    .scroll a{
-        color: #555 !important;
-        text-shadow: none;
-    }
-    .top-nav-content .menuNav{
-        font-weight: 100;
-        float:left;
-        margin:0;
-        padding:0;
-        box-sizing: border-box;
-        display: block;
-        text-shadow: 1px 1px 8px #444;
-    }
-    .top-nav-content .menuNav li{
-        display: block;
-        padding:20px 20px 20px 0px;
-        float:left;
-    }
-    .top-nav-content .menuNav a{
-        padding:15px;
-        line-height:20px;
-        text-decoration: none;
-        color: white;
-        font-size: 16px;
-        transition:all;
-    }
-    .top-nav-content .menuNav a:hover{
-        color:#0083cb;
-    }
-
-    .fade-enter-active, .fade-leave-active {
-        transition: opacity .5s;
-    }
-    .fade-enter, .fade-leave-to{
-        opacity: 0;
-    }
-</style>
--- src/main.js
+++ src/main.js
@@ -23,9 +23,6 @@ import swal from "sweetalert";
 //  第三方依赖——bootstrap
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-//	第三方依赖——代码美化
-import "./assets/dependence/prism/prism.css";
-import "./assets/dependence/prism/prism.js";
 
 //	关闭生产提示
 Vue.config.productionTip = false;
